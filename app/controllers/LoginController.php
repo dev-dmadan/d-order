d237326ea108ffaf6cb74fcf51fd108cd76823eb
Romadan Saputra <dev.rarasta27@gmail.com>
2019-02-13 15:35:08 +0700

    Update LoginController.php

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -71,7 +71,8 @@
                 $password = $dataUser['password'];
 
                 // if(($username === $this->username) && (password_verify($this->password, $password)) {
-                if(($username === $this->username) && ($password === $this->password)) {
+				if((($username === $this->username) && ($password === $this->password)) 
+					&& $dataUser['status_name'] === 'ACTIVE') {
                     $this->setSession($dataUser);
                     $this->success = true;
                 }
