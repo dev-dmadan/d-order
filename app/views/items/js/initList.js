41ad383c74e1be8a5ea51b4fb2e70efd98b0a5c7
Romadan Saputra <dev.rarasta27@gmail.com>
2019-02-13 18:45:03 +0700

    commit

--- app/views/items/js/initList.js
+++ app/views/items/js/initList.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
-	// event onclick btn add item
-    $('#btn-add-item').on('click', function() {
-        console.log("%cButton Add Order clicked...", "color: blue; font-style: italic");
-        showFormItem('action-add');
-    });
+    
 });
-
-/**
- *
- */
-function showFormItem(action) {
-	$('#modal-add-item').modal();
-}
