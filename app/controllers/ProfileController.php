d6d7153053ba4bbc4a6cf2f8dafb1771c96bfa75
Romadan Saputra <dev.rarasta27@gmail.com>
2019-02-15 09:46:49 +0700

    Create ProfileController.php

---
+++ app/controllers/ProfileController.php
@@ -0,0 +1,67 @@
+<?php
+
+    Defined("BASE_PATH") or die(ACCESS_DENIED);
+    
+    /**
+     * Class Profile
+     */
+    class Profile extends Controller
+    {
+        private $error = array();
+        private $notif = array();
+        private $message = NULL;
+        private $success = false;
+
+        /**
+         * Construct
+         * Load default library
+         */
+        public function __construct() {
+            $this->auth();
+            $this->auth->checkAuth();
+            $this->helper();
+            $this->validation();
+            $this->model('UserModel');
+            $this->model('DataTableModel');
+        }
+        
+        /**
+         * Method index
+         */
+        public function index() {
+            $this->detail();
+        }
+
+        /**
+         * 
+         */
+        private function detail() {
+            $config = array(
+                'title' => 'Profile',
+                'property' => array(
+                    'main' => 'User Profile', 'sub' => ''
+                ),
+                'css' => array(
+                    "assets/dist/modules/datatables/datatables.min.css",
+                    "assets/dist/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css",
+                    "assets/dist/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css",
+                ),
+                'js' => array(
+                    "assets/dist/modules/input-mask/jquery.inputmask.bundle.js",
+                    "assets/dist/modules/datatables/datatables.min.js",
+                    "assets/dist/modules/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js",
+                    "assets/dist/modules/datatables/Select-1.2.4/js/dataTables.select.min.js",
+                    "app/views/profile/js/initView.js"
+                ),
+            );        
+            $this->layout('profile/view', $config);
+        }
+
+        /**
+         * 
+         */
+        private function set_validation($data) {
+
+        }
+    }
+    
